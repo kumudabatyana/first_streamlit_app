2241f8b46d0e81e9ef51fd910c289fe015314d15
kumudabatyana <136359035+kumudabatyana@users.noreply.github.com>
2023-07-17 21:52:32 +0530

    Update streamlit_app.py
    space issue fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,15 +45,15 @@ except URLError as e:
 streamlit.header("The fruit load list contains:")
 #snowflake-related functions
 def get_fruit_load_list():
-with my_cnx.cursor() as my_cur
-my_cur.execute("SELECT * from fruit_load_list")
-retun my_cur.fetchall()
+    with my_cnx.cursor() as my_cur
+         my_cur.execute("SELECT * from fruit_load_list")
+         return my_cur.fetchall()
 
 #add a button to load the fruit
 if streamlit.button('Get Fruit Load List'):
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_data_rows = get_fruit_load_list()
-streamlit.dataframe(my_data_rows)
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    my_data_rows = get_fruit_load_list()
+    streamlit.dataframe(my_data_rows)
 #dont run anything past here while we troubleshoot
 streamlit.stop()
 #allow end user to add fruit 
